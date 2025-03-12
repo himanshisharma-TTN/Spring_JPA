0e8ea8b8691112e2687f5720bb1a87021c1438f7
himanshisharma-TTN <himanshisharma@tothenew.com>
2025-03-12 21:24:02 +0530

    Spring JPA 1

---
+++ SpringJPAExercise/Employee.java
@@ -0,0 +1,77 @@
+package com.product.SpringJPAExercise;
+
+import jakarta.persistence.*;
+
+//Ques1 - Create an Employee Entity which contains the following fields: Name, Id, Age, Location
+
+@Entity
+@Table(name = "Employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int age;
+
+    @Column(nullable = false)
+    private String address;
+
+    public Employee(){
+        this.name="Default Name";
+        this.age=0;
+        this.address="Default Address";
+    }
+
+    public Employee(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
